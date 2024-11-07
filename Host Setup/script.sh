d89781a4f77b4a6bad15f087ba4ac888667f2e60
Hunter O'Rourke <orourke.hunter@gmail.com>
2024-11-07 10:49:46 -0600

    Add setup script for pi control node

---
+++ Host Setup/script.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade -y
+apt install -y docker.io
+mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+systemctl enable --now kubelet
+apt-get install -y keepalived haproxy
