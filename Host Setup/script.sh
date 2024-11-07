de781f578c90ddf8f8da2e5306a59c9754b6d490
Kevin Carter <kevin.r.carter.mil@army.mil>
2024-11-07 11:07:42 -0600

    added uneeded things to script

--- Host Setup/script.sh
+++ Host Setup/script.sh
@@ -1,11 +1,22 @@
-apt update
-apt upgrade -y
-apt install -y docker.io
-mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
-systemctl enable --now kubelet
-apt-get install -y keepalived haproxy
+#!/bin/bash
+
+step_explain() {
+  echo -e "\n performing, $1 \n"
+  $1
+  echo -e "\n completed, $1 \n"
+}
+
+
+step_explain "apt update"
+step_explain "apt upgrade -y"
+step_explain "apt install -y docker.io"
+step_explain "mkdir -p -m 755 /etc/apt/keyrings"
+step_explain "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg"
+step_explain "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list"
+step_explain "apt-get update"
+step_explain "apt-get install -y kubelet kubeadm kubectl"
+step_explain "apt-mark hold kubelet kubeadm kubectl"
+step_explain "systemctl enable --now kubelet"
+step_explain "apt-get install -y keepalived haproxy"
+
+echo -e "Setup of Host complete"
